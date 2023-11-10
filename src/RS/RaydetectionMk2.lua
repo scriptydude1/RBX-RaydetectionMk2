664541f0c171f4e9c5e6dd3de5da8300df3c9fa6
scriptydude1 <scriptythememer@gmail.com>
2023-11-10 12:08:37 +0700

    basic functionality blueprint

--- src/RS/RaydetectionMk2.lua
+++ src/RS/RaydetectionMk2.lua
@@ -1,4 +1,30 @@
+local RunService = game:GetService("RunService")
+
 local Raydetection = {}
 Raydetection.__index = Raydetection
+
+function Raydetection._new(part)
+    local new = setmetatable({}, Raydetection)
+    
+    new.Parent = part
+    new.Length = 1
+    new.RayParams = RaycastParams.new()
+
+    return new
+end
+function Raydetection.newDirectional(part)
+    
+end
+function Raydetection.newOmnidirectional(part)
+    
+end
+
+function Raydetection:StartCast(frames)
+    
+end
+
+function Raydetection:Cleanup()
+    
+end
 
 return Raydetection
