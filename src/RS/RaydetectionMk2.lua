d9e7fec16d037208d76b261d2b88030c49ee31b7
scriptydude1 <scriptythememer@gmail.com>
2023-11-11 12:10:49 +0700

    using dictionary instead of an enum for CastDirections

--- src/RS/RaydetectionMk2.lua
+++ src/RS/RaydetectionMk2.lua
@@ -5,12 +5,13 @@ Raydetection.__index = Raydetection
 
 --Enum of a part CFrame directions
 Raydetection.CastDirEnum = {
-    "Front",
-    "Back",
-    "Left",
-    "Right",
-    "Up",
-    "Down"
+    ["Front"] = CFrame.new(0, 0, -1),
+    ["Back"] = CFrame.new(0, 0, 1),
+    ["Left"] = CFrame.new(-1, 0, 0),
+    ["Right"] = CFrame.new(1, 0, 0),
+    ["Down"] = CFrame.new(0, -1, 0),
+    ["Up"] = CFrame.new(0, 1, 0)
+
 }
 --[[
     Constructor methods
@@ -30,12 +31,12 @@ end
 function Raydetection.newDirectional(part, dir)
     local new = Raydetection._new(part)
 
-    if table.find(Raydetection.CastDirEnum, dir) then
+    if Raydetection.CastDirEnum[dir] then
     else
         dir = "Front"
         warn("Invalid direction when creating Raydetection.newDirectional")
     end
-    new.RayDir = dir
+    new.RayDir = Raydetection.CastDirEnum[dir]
 
     --Method that is called every frame in :StartCast()
     --Mainly for raycasting
