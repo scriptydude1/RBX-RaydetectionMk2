4ff4413d321f6e01a95ce30e4bf0a7c9147230ca
scriptydude1 <scriptythememer@gmail.com>
2023-11-11 14:21:05 +0700

    _fillAttach method for directional mode

--- src/RS/RaydetectionMk2.lua
+++ src/RS/RaydetectionMk2.lua
@@ -1,3 +1,4 @@
+--part.CFrame = part.CFrame * CFrame.new(0, 0, -5)
 local RunService = game:GetService("RunService")
 
 local Raydetection = {}
@@ -13,6 +14,20 @@ Raydetection.CastDirEnum = {
     ["Up"] = CFrame.new(0, 1, 0)
 
 }
+
+function Raydetection._fillAttach(part, volume)
+    --Grabs the half of the size, makes it negative, and then fills it out with attachments from one to another end
+    for x = (part.Size.X / 2) * -1, part.Size.X / 2, volume do
+        for y = (part.Size.Y / 2) * -1, part.Size.Y / 2, volume do
+            for z = (part.Size.Z / 2) * -1, part.Size.Z / 2, volume do
+                local attachment = Instance.new("Attachment")
+                attachment.Name = "RD"
+                attachment.CFrame = CFrame.new(x, y, z)
+                attachment.Parent = part
+            end
+        end
+    end
+end
 --[[
     Constructor methods
 ]]
@@ -28,20 +43,24 @@ function Raydetection._new(part)
 
     return new
 end
-function Raydetection.newDirectional(part, dir)
+function Raydetection.newDirectional(part, dir, attachVolume)
     local new = Raydetection._new(part)
+
+    new.FromPart = part
 
     if Raydetection.CastDirEnum[dir] then
     else
         dir = "Front"
-        warn("Invalid direction when creating Raydetection.newDirectional")
+        warn("Invalid direction when creating Raydetection.newDirectional. Switching to Front")
     end
     new.RayDir = Raydetection.CastDirEnum[dir]
+
+    new.Attachments = Raydetection._fillAttach(new.BasePart, attachVolume)
 
     --Method that is called every frame in :StartCast()
     --Mainly for raycasting
     function new:_Cast()
-        
+
     end
 
     return new
